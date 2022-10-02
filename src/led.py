0cbb375d16a0361cfc0b4ef095dffde95d409354
Matthew Katsenes <m.katsenes@gmail.com>
2022-10-02 12:19:10 -0400

    It can now set LED color based on averaging RGB values inside a given radius.

--- src/led.py
+++ src/led.py
@@ -32,6 +32,9 @@ class Led(object):
     def getColor(self):
         return (self.r,self.g,self.b)
     
+    def getColorBGR(self):
+        return (self.b,self.g,self.r)
+    
     def setPosition(self,x,y):
         self.x = x
         self.y = y
@@ -78,7 +81,6 @@ class LedString(object):
     def setColor(self,lightNum,r,g,b):
         self.stringOfLights[lightNum].setColor(r,g,b)
         
-
 class LedBoard(LedString):
     '''
     This class is meant to represent an LED board.
@@ -114,6 +116,27 @@ class LedBoard(LedString):
             x, y = led.getPosition()
             led.setColorArr(self.img[x][y])
             
+    def setLedColorCircle(self,ledNumber,radius):
+        '''
+        Sets the color of an individual LED to the average of the colors
+        in img based on a box of given size
+        '''
+        #convenience code
+        led = self.stringOfLights[ledNumber]
+        x, y = led.getPosition()
+        
+        #CIRCLE TIME:
+        #create a mask - all black image, white circle on top
+        circle_img = np.zeros(self.img.shape[:2], np.uint8)
+        cv2.circle(circle_img,(x,y),radius,(255,255,255), -1)
+        
+        #not sure why this produces a 4-item array, but we only need the first three.
+        #perhaps the 4th is the alpha channel?        
+        rgb_avg = cv2.mean(self.img,mask=circle_img)[0:3]
+        #print("led position x: ",x,", y: ",y)
+        #print("average rgb: ",rgb_avg)
+        
+        led.setColorArr(rgb_avg)
         
     def output(self,filepath="../boards/output.board"):
         '''
@@ -142,27 +165,47 @@ class LedBoard(LedString):
 #
 # for light in myString.stringOfLights:
 #     print(light)
-    
-myBoard = LedBoard(10,5,4)
 
-#randomize pixel color:
-myBoard.img = (np.random.rand(5,4,3) * 255).astype(np.uint8)
+#grab my green-blue stripe test image
+#gb = cv2.imread('../assets/gb_vert.jpg')
 
-#print(myBoard.img)
+
+#more complicated wavy image:
+wave = cv2.imread('../assets/wavy-stripes-2.jpg')
+#print("original image shape: ",wave.shape)
+
+
+myBoard = LedBoard(200,wave.shape[0],wave.shape[1])
+
+#converts cv2 default (BGR) to our format (RGB)
+myBoard.img = cv2.cvtColor(wave, cv2.COLOR_BGR2RGB)
+
+
+
+#randomize pixel color on image:
+#myBoard.img = (np.random.rand(myBoard.height,myBoard.width,3) * 255).astype(np.uint8)
 
 #randomize positions of LEDs for testing
 for led in myBoard.stringOfLights:
     led.setPosition(random.randrange(myBoard.height),random.randrange(myBoard.width))
-    
 
-myBoard.setLedColorsStrict()
 
+#myBoard.setLedColorCircle(0, 10)
+#set all the LED colors.
+for i in range(myBoard.numLights):
+    myBoard.setLedColorCircle(i, 10)
+
+
+# #set the led colors to only what's on top of them    
+# myBoard.setLedColorsStrict()
+#
+#draw a circle around the where the LEDs are  in the image
 for led in myBoard.stringOfLights:
-    print(led)
+    cv2.circle(wave,led.getPosition(),10,led.getColorBGR(),-1)    
 
-# cv2.imshow('test',myBoard.img)
-# k = cv2.waitKey(0)
-# if k == 27 or k == ord('q'):
-#     cv2.destroyAllWindows()
+cv2.imshow('test',wave)
+k = cv2.waitKey(0)
+if k == 27 or k == ord('q'):
+    cv2.destroyAllWindows()
 
 #myBoard.output("../boards/test.board")
