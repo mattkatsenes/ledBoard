e11dc2918f08cc440839d1390c7cf674a8bc469c
Mary <marycotrupi@gmail.com>
2022-10-08 16:43:25 -0400

    Added 'stripes' function with RGB param

--- src/mary.py
+++ src/mary.py
@@ -12,7 +12,7 @@ def stripes(aBoard, r, g, b):
     for led in aBoard.stringOfLights:
         y = led.y
         h = aBoard.height
-        if y < h/5 or y > h-h/5:
+        if y <= 4/5*h and y>= 3/5*h or y <=2/5*h and y >= 1/5*h:
             led.setColor(r, g, b)
     #aBoard.output('../boards/stripes.board')
 
@@ -24,10 +24,9 @@ myBoard = led.LedBoard(200,wave.shape[0],wave.shape[1])
 for led in myBoard.stringOfLights:
     led.setPosition(random.randrange(myBoard.height),random.randrange(myBoard.width))
 
-stripes(myBoard, 255, 0, 255)
+stripes(myBoard, 100, 100, 255)
 for index, led in enumerate(myBoard.stringOfLights):
     cv2.circle(wave,led.getPosition(),20,led.getColorBGR(),-1)    
-print('hi')
 cv2.imshow('test',wave)
 k = cv2.waitKey(0)
 if k == 27 or k == ord('q'):
