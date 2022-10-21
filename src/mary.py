5ef36b16fe8323e9545e767dd2e628fea8e9ae6f
Mary <marycotrupi@gmail.com>
2022-10-21 11:53:16 -0400

    turn lights off

--- src/mary.py
+++ src/mary.py
@@ -14,11 +14,6 @@ def animateLine(aBoard, i, r, g, b):
         x = led.x
         if x >= i / 100 *w and x <= (i+1) / 100 *w:
             led.setColor(r, g, b)
-            #cv2.circle(wave,led.getPosition(),20,led.getColorBGR(),-1)    
-            #cv2.imshow('test',wave)
-            #k = cv2.waitKey(0)
-            #if k == 27 or k == ord('q'):
-             #   cv2.destroyAllWindows()
 
 linuxPath = "../assets/"
 marysPath = "c:/Users/maryc/OneDrive/Desktop/ledBoard/assets/"
@@ -29,12 +24,13 @@ def stripes(aBoard, r, g, b):
         h = aBoard.height
         if y <= 4/5*h and y>= 3/5*h or y <=2/5*h and y >= 1/5*h:
             led.setColor(r, g, b)
-    #aBoard.output('../boards/stripes.board')
+        else:
+            led.setColor(0,0,0)
 
-wave = cv2.imread('c:/Users/maryc/OneDrive/Desktop/ledBoard/assets/wavy-stripes-2.jpg')
+wave = cv2.imread('assets/wavy-stripes-2.jpg')
 
 
-myBoard = led.LedBoard(200,wave.shape[0],wave.shape[1])
+myBoard = LedBoard(200,wave.shape[0],wave.shape[1])
 for led in myBoard.stringOfLights:
     led.setPosition(random.randrange(myBoard.height),random.randrange(myBoard.width))
 
